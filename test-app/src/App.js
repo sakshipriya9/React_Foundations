0c040f9ec76e0a03ad9d7a4baae3ffdd019d8ec5
Sakshi Priya <priyasakshi8989@gmail.com>
2024-05-04 18:04:03 +0530

    Learning React

---
+++ test-app/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome To React App</h1>
+      <h2> Learning react is a beneficial to make an app!!!</h2>
+    </div>
+  );
+}
+
+export default App;
